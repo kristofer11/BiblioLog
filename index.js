b6104dd8814feaf48d452b6d9ae7c36b8c6878c1
Kris Hvattum <klhvattum@live.com>
2023-02-06 22:43:02 -0800

    Button to 'mark as read'. Toggles between read and not read

--- index.js
+++ index.js
@@ -1,20 +1,5 @@
-//Sample array to work with during development
-let myLibrary = [
-    {
-        id: 0,
-        title: "The Fellowship of the Rings",
-        author: "Tolkien",
-        pages: 1100,
-        read: true
-    },
-    {
-        id: 1,
-        title: "The Silver Chair",
-        author: "Lewis",
-        pages: 222,
-        read: true
-    }
-];
+//Array of books to display.
+let myLibrary = [];
 
 const libraryDisplay = document.getElementById('libraryDisplayContainer');
 
@@ -31,6 +16,17 @@ class Book {
         this.author = author
         this.pages = pages
         this.read = read
+    }
+};
+
+//Creates method to toggle book as read. NOT WORKING YET
+Book.prototype.toggleRead = function(){
+    if (this.read) {
+        this.read = false
+        console.log(`${this.title} has not been read`)
+    } else {
+        this.read = true;  
+        console.log(`${this.title} has been read`)
     }
 };
 
@@ -55,6 +51,7 @@ submitButton.addEventListener('click', (e) => e.preventDefault());
 submitButton.addEventListener('click', () => {
     addBook(titleInput.value, authorInput.value, pagesInput.value);
     resetInputs();
+    closeModal(modal);
 });
 
 //Reset form after submission
@@ -80,9 +77,12 @@ const populateLibrary = () => {
         const title = bookItem.appendChild(document.createElement('div'));
         const author = bookItem.appendChild(document.createElement('div'));
         const pages = bookItem.appendChild(document.createElement('div'));
-        // const read = bookItem.appendChild(document.createElement('div'));
+        const readStatus = bookItem.appendChild(document.createElement('div'));
+        const readBtn = bookItem.appendChild(document.createElement('button'));
         const deleteBtn = bookItem.appendChild(document.createElement('button'));
         deleteBtn.textContent = 'Delete Book';
+        deleteBtn.classList.add('deleteBtn');
+        readBtn.textContent = 'Mark as "read"';
 
         title.textContent = `Title: ${book.title}`;
         author.textContent = `Author: ${book.author}`;
@@ -90,20 +90,23 @@ const populateLibrary = () => {
 
         deleteBtn.addEventListener('click', (e) => {
             e.preventDefault();
-    //THIS SEEMS TO WORK NOW, KEEP TESTING!
             removeBook(book.title);
-            console.log(book.title)
         });
 
+        readBtn.addEventListener('click', (e) => {
+            e.preventDefault();
+            book.toggleRead();
+            book.read ? readStatus.textContent = 'READ' : readStatus.textContent = 'NOT READ'
+
+        })
     })
 };
 
+//Remove book from display AND array
 const removeBook = (title) => {
 
     const thisBook = document.getElementById(`${title}`);
     console.log(`${title} was removed from your library`);
-
-//THIS SEEMS TO WORK NOW! KEEP TESTING!
     myLibrary.forEach((book, index) => {
         if(book.title === title) {
             myLibrary.splice(index, 1)
@@ -114,6 +117,9 @@ const removeBook = (title) => {
     console.log(myLibrary);
 };
 
+
+
+//Opening and closing "Add Book" modal
 const openModalButton = document.querySelector('#openModalButton');
 const closeModalButton = document.querySelector('#closeModalButton');
 const overlay = document.querySelector('#overlay');
@@ -127,8 +133,9 @@ overlay.addEventListener('click', () => {
     const modals = document.querySelectorAll('.modal.active');
     modals.forEach(modal => {
         closeModal(modal)
-    })
-})
+    });
+    resetInputs();
+});
 
 closeModalButton.addEventListener('click', () => {
     const modal = document.querySelector('#modal')
@@ -153,7 +160,7 @@ console.log(myLibrary);
 
 
 // NEXT STEPS:
-// 1. INSTEAD OF THE FORM ALWAYS BEING ON THE top, MAKE IT A BUTTON THAT BRINGS UP A MODAL CONTAINING THE FORM.
-// 2. ADD THE ABILITY TO CHANGE THE READ STATUS OF EACH Book.
+// ---DONE --- 1. INSTEAD OF THE FORM ALWAYS BEING ON THE top, MAKE IT A BUTTON THAT BRINGS UP A MODAL CONTAINING THE FORM.
+// ---DONE --- 2. ADD THE ABILITY TO CHANGE THE READ STATUS OF EACH Book.
 // 3. STYLING: MAKE IT SNAZZY...BOOTSTRAP?
 // 4. FIGURE OUT LOCAL Storage. FIND THE VIDEO USED LAST TIME IF NEEDED ( KEEP READING THROUGH THE ODIN PROJECT AND COMPLETE THIS STEP AT THE APPROPRIATE TIME)
